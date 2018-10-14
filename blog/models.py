5bdada860f79a2367dfa74e6e71a8820b4f2212e
anuja <anujajadhav636@gmail.com>
2018-10-14 14:25:16 +0530

    second commit

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    "creating models for post"
+    
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
